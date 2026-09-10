5be40a9e9f924081e49ecbf5c10dcc1b6f47ed6f
Ipe051106 <philipraflores0511@gmail.com>
2026-09-10 21:03:33 +0800

    feat: create responsive navbar

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
-        ({
+        laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
